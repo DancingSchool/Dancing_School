a9c4055a0557db9260817af7e5eeaf188de9cd7f
za2e13 <za2e13@soton.ac.uk>
2014-11-26 15:45:52 +0000

    couple page updated

--- student_couple.php
+++ student_couple.php
@@ -30,6 +30,56 @@
 							</div>
 						</div>
 						
+						
+						
+						<?php
+ 							if(isset($_POST['formSubmit']) )
+							{
+							  $varMovie = $_POST['formMovie'];
+							  $varName = $_POST['formName'];
+							  $varGender = $_POST['formGender'];
+							  $errorMessage = "thaks";
+							 
+							  
+							}
+							 
+						?>
+						
+						<div class="form-group">
+							<p class="col-sm-2 text-right control-lable" id="femaleID">Select Dance Type :</p>
+							<div class="col-sm-6">
+								<div class="col-sm-6">
+									<p id="dances"> </p>
+								<select name ="instID" onchange ="revealChoice(this.value)" class="form-control col-sm-2 text-right control-lable">
+									<option>----</option>
+									<option value="ballroom">Ballroom</option>
+									<option value="latin american">Latin American</option>
+
+								</select>								
+							</div>
+							</div>
+							
+							
+							<div class="col-md-6">
+								<div class="col-md-12">
+									<form name="ballroom_option" class="container">
+										<input type="checkbox" name="waltz" value="waltz"> Waltz
+										<input type="checkbox" name="foxtrot" value="foxtrot"> Foxtrot
+										<input type="checkbox" name="quickstep" value="quickstep"> Quick Step
+										<input type="checkbox" name="tango" value="tango"> Tango
+									</form>	
+									
+									<form name="LatinAmerican_option">
+										<input type="checkbox" name="chacha" value="chacha"> Cha Cha
+										<input type="checkbox" name="rumba" value="rumba"> Rumba
+										<input type="checkbox" name="samba" value="samba"> Samba
+										<input type="checkbox" name="jive " value="jive "> Jive 
+										<input type="checkbox" name="pasodoble" value="pasodoble"> Paso Doble
+									</form>						
+								</div>
+							</div>
+						</div>
+						
 						<div class="form-group">
 							<div class="col-sm-offset-2 col-sm-6">
 								<button type="submit" class="btn btn-primary">Submit</button>
